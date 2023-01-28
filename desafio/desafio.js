7fa30537a6d0a50a8b0c754237fcfcb891d8045e
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-01-28 12:12:47 -0300

    Feito o desafio da lista de tintas

---
+++ desafio/desafio.js
@@ -0,0 +1,20 @@
+const lista = document.querySelector(".lista");
+const botoes = document.querySelectorAll("[data-mostrar]");
+
+botoes.forEach((botao) => {
+    botao.addEventListener("click", (evento) => {
+        const atributo = verificar(evento.target.dataset.mostrar)
+        mudarDisplay(evento.target, atributo)
+    })
+})
+
+function verificar (elemento){
+    if (elemento === "mostrar"){
+        return "block"
+    }else if (elemento === "esconder") {
+        return "none"
+    }
+}
+function mudarDisplay (elemento, valor){
+    lista.style.display = valor;
+}
